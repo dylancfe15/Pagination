e4eb49e870f5ea79eba60cef4f0cd30f58377f64
Dylan Chen <dylancfe15@gmail.com>
2022-05-10 00:46:19 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions")
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+const firestore = admin.firestore()
+
+exports.getStocks = functions.https.onCall(async (request, context) => {
+    const { currentCount, stortedBy } = request
+    const limit = 10
+    const endIndex = currentCount - 1
+    stortedBy = stortedBy == null ? 'symbol' : stortedBy
+
+    try {
+        let reference = firestore.collection('Stocks')
+        let stocks = await reference.orderBy(stortedBy).get()
+
+        if (stortedBy != null) {
+            return stocks.slice(endIndex)
+        } else {
+            return stocks.slice(endIndex, endIndex+limit)
+        }
+    } catch(error) {
+        console.log(error)
+
+        return { success: false }
+    }
+})
